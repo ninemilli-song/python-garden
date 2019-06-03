d5a142cb62ea013bf1e9a856b45ff26afdb55c79
ninemilli.song <ninemilli_song@163.com>
2019-06-03 10:05:42 +0800

    添加代码

--- sorting_algorithms/main.py
+++ sorting_algorithms/main.py
@@ -59,3 +59,24 @@ def selection_sort(arr):
         arr[minimum], arr[i] = arr[i], arr[minimum]
 
     return arr
+
+
+def insertion_sort(arr):
+    """
+    在每个循环迭代中，插入排序从数组中删除一个元素。
+    然后，它在另一个排序数组中找到该元素所属的位置，并将其插入其中。
+    它重复这个过程，直到没有输入元素。
+    :param arr:
+    :return:
+    """
+    for i in range(len(arr)):
+        cursor = arr[i]
+        pos = i
+
+        while pos > 0 and arr[pos - 1] > cursor:
+            arr[pos] = arr[pos - 1]
+            pos = pos - 1
+
+        arr[pos] = cursor
+
+    return arr
