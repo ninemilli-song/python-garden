5b8b7e6c99c7a80f1a07817f31aa213a5629d427
ninemilli.song <ninemilli_song@163.com>
2019-07-05 13:44:02 +0800

    字符串练习

---
+++ practice/bulletPointAdder.py
@@ -0,0 +1,16 @@
+#!python3
+# bulletPointAdder.py - Adds Wikipedia bullet to the start
+# of each line of text on the clipboard
+
+import pyperclip
+
+
+text = pyperclip.paste()
+
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '* ' + lines[i]
+
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
